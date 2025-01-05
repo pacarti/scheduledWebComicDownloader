475d5d74f0c8d13bc99c041408dbf8a4d323c9b9
pacarti <a.paciorkowski@protonmail.com>
2025-01-06 00:19:46 +0100

    Added saving the last downloaded comic ID into a text file and further, checking if it's still the ID of the latest comic by the next link

--- scheduledWebComicDownloader.py
+++ scheduledWebComicDownloader.py
@@ -4,52 +4,79 @@
 # Step1: start at 3028 and download until 3033
 # Step2: Store the id number of currently latest comic(e.g. 3033) in a text file
 # Step3: Check the 'Next' link. If it doesn't end with '#' then download new comics. Else there are no new comics 
+# ...
+# do also for Qwantz
 
 import requests, os, bs4
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+def downloadXkcdComics():
 
-url = 'https://xkcd.com/3032/'                # starting url    
+    latestComicIDFile = open('latestXkcdComicID.txt')
+    latestComicID = latestComicIDFile.read()
 
-os.makedirs('xkcd', exist_ok=True)            # store comics in ./xkcd
+    url = 'https://xkcd.com/' + latestComicID     # starting url
+    # url = 'https://xkcd.com/3032'
 
-while not url.endswith('#'):
     # Download the page:
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    # Find the URL of the comic image:
-    comicElem = soup.select('#comic img')
-    if comicElem == []:
-        print('Could not find comic image.')
-    else:
-        comicUrl = 'https:' + comicElem[0].get('src')
-    # Download the image:
-        print('Downloading image %s...' % (comicUrl))
-        res = requests.get(comicUrl)
+
+    nextLinkHref = soup.select('a[rel="next"]')[0].get('href')
+
+    if nextLinkHref == '#':
+        print("No new comics since last download. Exiting...")
+        exit()
+
+    os.makedirs('xkcd', exist_ok=True)            # store comics in ./xkcd
+
+    while not url.endswith('#'):
+        # Download the page:
+        print('Downloading page %s...' % url)
+        res = requests.get(url)
         res.raise_for_status()
-    # Save the image to ./xkcd:
-        imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
-        for chunk in res.iter_content(100000):
-            imageFile.write(chunk)
-        imageFile.close()
-    # Get the Next button's url:
-    nextLink = soup.select('a[rel="next"]')[0]
-    currentUrl = url
-    url = 'https://xkcd.com' + nextLink.get('href')
-    # Last link is: ''https://xkcd.com#''
+        soup = bs4.BeautifulSoup(res.text, 'html.parser')
+        # Find the URL of the comic image:
+        comicElem = soup.select('#comic img')
+        if comicElem == []:
+            print('Could not find comic image.')
+        else:
+            comicUrl = 'https:' + comicElem[0].get('src')
+        # Download the image:
+            print('Downloading image %s...' % (comicUrl))
+            res = requests.get(comicUrl)
+            res.raise_for_status()
+        # Save the image to ./xkcd:
+            imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+            for chunk in res.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+        # Get the Next button's url:
+        nextLink = soup.select('a[rel="next"]')[0]
+        currentUrl = url
+        url = 'https://xkcd.com' + nextLink.get('href')
+        # Last link is: ''https://xkcd.com#''
 
-# e.g. url = https://xkcd.com/3033/#
-# Remove '/' from the link to save the latest comic ID:
-currentUrl = currentUrl[:-1]
-# print(currentUrl)
-latestComicID = os.path.basename(currentUrl)
-# print(latestComicID)
+    # e.g. url = https://xkcd.com/3033/#
+    # Remove '/' from the link to save the latest comic ID:
+    currentUrl = currentUrl[:-1]
+    # print(currentUrl)
+    latestComicID = os.path.basename(currentUrl)
+    # print(latestComicID)
 
 
-latestComicIDFile = open('latestComicID.txt', 'w')
-latestComicIDFile.write(latestComicID)
-latestComicIDFile.close()
+    latestComicIDFile = open('latestXkcdComicID.txt', 'w')
+    latestComicIDFile.write(latestComicID)
+    latestComicIDFile.close()
+
+def main():
+
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    downloadXkcdComics()
+
+if __name__ == "__main__":
+    main()
 
 print('Done.')
